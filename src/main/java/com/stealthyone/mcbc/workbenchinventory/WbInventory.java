8e8177b83befafa47580aef0ab11b88eae5dfaa5
Stealth2800 <stealth2800@stealthyone.com>
2014-06-30 00:13:10 -0700

    Updated version [1.1.0] Added autosaving.

--- src/main/java/com/stealthyone/mcbc/workbenchinventory/WbInventory.java
+++ src/main/java/com/stealthyone/mcbc/workbenchinventory/WbInventory.java
@@ -1,9 +1,11 @@
 package com.stealthyone.mcbc.workbenchinventory;
 
+import com.stealthyone.mcb.stbukkitlib.autosaving.Autosavable;
+import com.stealthyone.mcb.stbukkitlib.autosaving.Autosaver;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class WbInventory extends JavaPlugin {
+public class WbInventory extends JavaPlugin implements Autosavable {
 
     private InventorySaver inventorySaver;
 
@@ -14,8 +16,12 @@ public class WbInventory extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         Bukkit.getPluginManager().registerEvents(inventorySaver = new InventorySaver(this), this);
         inventorySaver.load();
+        if (!Autosaver.scheduleForMe(this, this, getConfig().getInt("Autosave interval", 0))) {
+            getLogger().warning("Autosaving disabled.  It is recommended that you enable it to prevent data loss!");
+        }
         getLogger().info("WorkbenchInventory v" + getDescription().getVersion() + " by Stealth2800 ENABLED.");
     }
 
